d3a79839367c590daccb16096bf68b4b39a7a397
JeongHM <kjhm0607@gmail.com>
2020-11-29 12:21:30 +0900

    [feat] 내적 풀이

---
+++ programmers/level1/min/내적.py
@@ -0,0 +1,12 @@
+def solution(a: list, b: list) -> int:
+    result = list()
+
+    for num_a, num_b in zip(a, b):
+        number = num_a * num_b
+        result.append(number)
+    return sum(result)
+
+
+def other_solution(a: list, b: list) -> int:
+    return sum([num_a * num_b for num_a, num_b in zip(a, b)])
+
