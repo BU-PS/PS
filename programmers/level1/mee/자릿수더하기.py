dff8b91d749adfd5c19f1e873f1bcdd7f632fcd1
LJM980 <devljm98@gmail.com>
2020-12-21 21:45:21 +0900

    LV1 자릿수더하기

--- programmers/level1/mee/자릿수더하기.py
+++ programmers/level1/mee/자릿수더하기.py
@@ -1,6 +1,13 @@
 def solution(n):
-    sum=0
-    while(n!=0):
-        sum+=n%10
-        n=n//10
-    return sum
+#수정
+    answer=[int(i) for i in str(n)]
+    return sum(answer)
+
+    
+    # sum=0
+    # while(n!=0):
+    #     sum+=n%10
+    #     n=n//10
+    # return sum
+
+    
