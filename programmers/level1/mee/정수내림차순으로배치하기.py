49dd2087fea7c8741b0d07aa1f12fe857604d4e5
LJM980 <devljm98@gmail.com>
2020-12-21 21:47:07 +0900

    LV1 정수 내림차순으로 배치하기

--- programmers/level1/mee/정수내림차순으로배치하기.py
+++ programmers/level1/mee/정수내림차순으로배치하기.py
@@ -1,5 +1,10 @@
 def solution(n):
+    #수정
     answer=list(str(n))
-    answer.sort()
-    answer.reverse()
-    return int("".join(answer))
+    return int(''.join(sorted(answer,reverse=False)))
+
+    
+    # answer=list(str(n))
+    # answer.sort()
+    # answer.reverse()
+    # return int("".join(answer))
