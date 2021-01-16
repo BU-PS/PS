8da66a43c9ef4d9884f65c42e014c942147bf1de
LJM980 <devljm98@gmail.com>
2021-01-17 00:11:36 +0900

    [1차] 비밀지도

---
+++ programmers/level1/mee/8.[1차]비밀지도.py
@@ -0,0 +1,13 @@
+# arr값 각각 2진수로 만들기
+# arr1과 arr2 합치기
+# 합친 값에서 1인곳은 #으로 0인곳은 공백으로 표현하여 리턴
+def solution(n, arr1, arr2):
+    answer=[] 
+    for i in range(n):
+        arr=bin(arr1[i] | arr2[i]) # arr1과 arr2를 2진수로 만들어서 합쳐주기(비트연산자)
+        arr=arr[2:] 
+        if len(arr)<n:
+            arr='0'*(n-len(arr))+arr #잊어버리지말기..
+        arr=arr.replace('0',' ').replace('1','#')
+        answer.append(arr)
+    return answer
