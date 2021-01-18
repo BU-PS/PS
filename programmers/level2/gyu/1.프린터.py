d44e2737908ee4190f0ddc51aa581b326cb63e4e
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-01-19 04:49:07 +0900

    LV2 프린터

--- programmers/level2/gyu/1.프린터.py
+++ programmers/level2/gyu/1.프린터.py
@@ -1,1 +1,15 @@
-# level2 초기 셋팅
+def solution(priorities, location):
+    answer = 0
+    priorities = [(priority, idx)for idx, priority in enumerate(priorities) ]    
+
+    while True:
+        if max(priorities)[0] == priorities[0][0]:      # 가장 큰 값 찾기
+            answer += 1
+            if priorities[0][1] == location:            # 내가 찾고자 하는 경우인지
+                break
+            else:
+                priorities.pop(0)
+        else:
+            priorities.append(priorities.pop(0))        # 가장 큰 값 아니면 맨 뒤로 보내기 
+
+    return answer
