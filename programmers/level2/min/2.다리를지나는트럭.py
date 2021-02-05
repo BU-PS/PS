d5f6f0b6c1433582dca927524b366d208183ceed
JeongHM <kjhm0607@gmail.com>
2021-02-05 18:32:58 +0900

    LV2 다리를 지나는 트럭

--- programmers/level2/min/2.다리를지나는트럭.py
+++ programmers/level2/min/2.다리를지나는트럭.py
@@ -1,16 +1,46 @@
-def solution(bridge_length: int, weight: int, truck_weights: list):
-    time = 0
-    bridge_weights = [0 for i in range(bridge_length)]
+import unittest
 
-    while truck_weights:
-        bridge_weights.pop(0)
 
-        if sum(bridge_weights) + truck_weights[0] <= weight:
-            bridge_weights.append(truck_weights.pop(0))
+def solution(bridge_length: int, weight: int, truck_weights: list) -> int:
+    answer = 0
+
+    bridge_in_truck = [0] * bridge_length
+
+    while True:
+
+        if not truck_weights:
+            answer += len(bridge_in_truck)
+            break
+
+        answer += 1
+
+        bridge_in_truck.pop(0)
+        bridge_in_truck_weight = sum(bridge_in_truck)
+
+        truck_weight = truck_weights[0]
+
+        if bridge_in_truck_weight + truck_weight <= weight:
+            bridge_in_truck.append(truck_weights.pop(0))
+
         else:
-            bridge_weights.append(0)
-        time += 1
+            bridge_in_truck.append(0)
 
-    loading = len(bridge_weights)
+    return answer
 
-    return time + loading
+class SolutionTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self._bridge_length = 2
+        self._weight = 10
+        self._truck_weights = [7, 4, 5, 6]
+        self._result = 8
+
+    def test_solution(self):
+        result = solution(bridge_length=self._bridge_length,
+                          weight=self._weight,
+                          truck_weights=self._truck_weights)
+
+        self.assertEqual(self._result, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
