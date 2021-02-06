032f24f06e9cecb963819e1697ccf4bbe123d79a
LJM980 <devljm98@gmail.com>
2021-02-06 19:03:17 +0900

    LV2 다리를 지나는 트럭

--- programmers/level2/mee/2.다리를지나는트럭.py
+++ programmers/level2/mee/2.다리를지나는트럭.py
@@ -1,4 +1,6 @@
 # 다리길이와 건너는시간은 같음.
+# 다리의 현재 상태를 표현해야함.
+# 트럭이 지나고있는동안 다리위에 무게의 합이 weight보다 작다면 다음 트럭을 넣어주고 아니라면 0을 넣어주기.
 def solution(bridge_length, weight, truck_weights):
     answer = 0
     br_tr=[0 for i in range(bridge_length)]
@@ -6,8 +8,8 @@ def solution(bridge_length, weight, truck_weights):
         answer+=1 
         br_tr.pop(0) 
         if truck_weights:
-            if sum(br_tr)+truck_weights[0] <= weight:
+            if sum(br_tr)+truck_weights[0] <= weight: 
                 br_tr.append(truck_weights.pop(0)) 
             else: 
-                br_tr.append(0)  
+                br_tr.append(0) 
     return answer
